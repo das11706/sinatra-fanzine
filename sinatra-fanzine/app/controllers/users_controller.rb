6d1165c31964b02312a12041d6a9b9b8064ba1da
Daniel Suarez <suarez.daniel101@gmail.com>
2022-05-12 11:53:36 -0400

    created get 'users' and 'signup' controllers

--- sinatra-fanzine/app/controllers/users_controller.rb
+++ sinatra-fanzine/app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
 
+  get '/users' do
+    @users = User.all 
+    erb :'/users/show'
+  end
+
+  get '/signup' do
+    if !logged_in?
+      erb :'/users/create_user'
+    else 
+      redirect '/tweets'
+    end
+  end
+
 end
