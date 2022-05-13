d54212566e57f36b7a1fba3e06cb132c14d4c305
Daniel Suarez <suarez.daniel101@gmail.com>
2022-05-13 16:28:03 -0400

    working on controllers

--- sinatra-fanzine/app/controllers/users_controller.rb
+++ sinatra-fanzine/app/controllers/users_controller.rb
@@ -9,7 +9,18 @@ class UsersController < ApplicationController
     if !logged_in?
       erb :'/users/create_user'
     else 
-      redirect '/tweets'
+      redirect '/zines'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id 
+        redirect '/zines'
     end
   end
 
--- sinatra-fanzine/app/controllers/zines_controller.rb
+++ sinatra-fanzine/app/controllers/zines_controller.rb
@@ -1,3 +1,66 @@
 class ZinesController < ApplicationController
+  
+  get '/zines' do
+    @zines = Zine.all
+    if logged_in?
+      erb :'/zines/zines'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/zines/new' do
+    if logged_in?
+      users = User.all
+      erb :'/zines/new'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/zines/:id' do
+    if logged_in?
+      @zine = Zine.find_by(params[:title])
+      erb :'/zines/show_zine'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/zines/:id/edit' do
+    if logged_in?
+      @zine = Zine.find_by(params[:title])
+      erb :'/zines/edit_zine'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/zines' do
+    if logged_in?
+      if params[:title] == "" || params[:creator] == "" || params[:content] == ""
+        redirect '/zines/new'
+      else
+        @zine = current_user.zines.create(params[:zine])
+        redirect '/zines/:id'
+      end
+    end
+  end
+
+  patch '/zines/:id' do
+    if logged_in?
+      @zine = Zine.find_by(params[:title])
+      @zine.update(params[:zine])
+      redirect "/zines/#{@zine.id}/edit"
+    else
+      redirect '/login'
+    end
+  end
+
+  delete '/zines/:id' do
+    @zine = Zine.find_by(params[:title])
+    @zine.delete
+    redirect to '/zines'
+  end
 
 end
